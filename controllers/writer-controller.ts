d7ac93972154671e7915f67cf4d06d311f727cea
Ho Van Son <88260562+sonhv3112@users.noreply.github.com>
2023-07-04 16:32:02 +0700

    complete writer

--- controllers/writer-controller.ts
+++ controllers/writer-controller.ts
@@ -35,67 +35,62 @@ export class WriterController {
         })
     }
 
-    async getLatestStatusOfArticle(articleId: number): Promise<ArticleStatus> {
-        const result = await this.statusRepository.find({
-            where: {
-                article: {id: articleId}
-            },
-            relations: {
-                performer: true
-            },
-            order: {
-                time: "DESC"
-            },
-            take: 1,
-            cache: true,
+    async getLatestStatusOfArticles(articleIds: number[]): Promise<ArticleStatus[]> {
+        const promises = articleIds.map(async (id) => {
+            return this.statusRepository.findOneBy({ id });
         });
-        if (result.length > 0) {
-            return result[0];
-        } else {
-            return null;
-        }
+    
+        const results = await Promise.all(promises);
+        return results;
     }
 
     // Based on Specification
     async uploadArticle(authorID: number, title: string, summary: string, content: string,
-        categoryName: string, tagList: string[], premium = false): Promise<void> {
+        categoryName: string, tagList: string[], premium = false): Promise<object> {
 
-        const newArticle = new Article();
-        const newStatus = new ArticleStatus();
+        try { 
+            const newArticle = new Article();
+            const newStatus = new ArticleStatus();
 
-        const user = await this.userController.getUserById(authorID);
-        let category = await this.categoryController.getCategoryByName(categoryName);
-        // if category == null -> create new category
-        if (category == null) {
-            category = await this.categoryController.createCategory(categoryName, "", null);
-        }
-
-        const tags = await Promise.all(tagList.map(async tagName => {
-            const t = await this.tagController.getTagByName(tagName);
-            if (t == null) {
-                return await this.tagController.createTag(tagName, "");
-            } else {
-                return t;
+            const user = await this.userController.getUserById(authorID);
+            let category = await this.categoryController.getCategoryByName(categoryName);
+            // if category == null -> create new category
+            if (category == null) {
+                category = await this.categoryController.createCategory(categoryName, "", null);
             }
-        }));
 
-        newArticle.author = user;
-        newArticle.category = category;
-        newArticle.content = content;
-        newArticle.is_premium = premium;
-        newArticle.short_description = summary;
-        newArticle.title = title;
-        newArticle.tags = tags;
-        newArticle.thumbnail_url = "https://www.solidbackgrounds.com/images/1280x720/1280x720-black-solid-color-background.jpg"; // Hardcoded image
-        
-        await this.articleRepository.save(newArticle);
+            const tags = await Promise.all(tagList.map(async tagName => {
+                const t = await this.tagController.getTagByName(tagName);
+                if (t == null) {
+                    return await this.tagController.createTag(tagName, "");
+                } else {
+                    return t;
+                }
+            }));
+ 
+            newArticle.author = user;
+            newArticle.category = category;
+            newArticle.content = content;
+            newArticle.is_premium = premium;
+            newArticle.short_description = summary;
+            newArticle.title = title;
+            newArticle.tags = tags;
+            newArticle.thumbnail_url = "https://www.solidbackgrounds.com/images/1280x720/1280x720-black-solid-color-background.jpg"; // Hardcoded image
+            
+            const saveArticleResult = await this.articleRepository.save(newArticle);
 
-        newStatus.article = newArticle;
-        newStatus.note = "Created"
-        newStatus.performer = user;
-        newStatus.status = StatusList.DRAFT;
-        
-        await this.statusRepository.save(newStatus);
+            newStatus.article = newArticle;
+            newStatus.note = "Created"
+            newStatus.performer = user;
+            newStatus.status = StatusList.DRAFT;
+            
+            const saveArticleStatusResult = await this.statusRepository.save(newStatus);
+
+            return {"status": 200, "message": "OK"}; 
+        } catch (error) { 
+            console.error(`Failed to get user by email: ${error.message}`);
+            return {"status": 200, "message": error.message}; 
+        }
     }
 
     async editArticle(articleId: number, title: string, summary: string, content: string,
--- routes/writer-route.ts
+++ routes/writer-route.ts
@@ -1,72 +1,102 @@
-import { Router, Request, Response } from "express";
+import express, { Router, Request, Response } from "express";
 import { footerGenerator, headerGenerator } from "../utils/header-footer-generator";
 import { CategoryController } from "../controllers/category-controller";
 import { UserMiddleware } from "../controllers/middleware/user-middleware";
 import { Category } from "../models/category";
 import { TagController } from "../controllers/tag-controller";
+import { WriterController } from "../controllers/writer-controller";
+import { write } from "fs";
 
 const router: Router = Router();
 
-const userMiddleware = new UserMiddleware(); 
+router.use(express.json());
+
+const userMiddleware = new UserMiddleware();
 
 router.get("/", userMiddleware.authenticate, async (req: Request, res: Response) => {
-    const categoryController = new CategoryController();
+    const writerController = new WriterController();
 
     // @ts-ignore
     console.log(req.jwtObj)
 
     // @ts-ignore
-    if (req.isAuth) { 
+    if (req.isAuth) {
         // @ts-ignore
-        const role = req.jwtObj.role.name; 
-        if (role == "writer") { 
+        const role = req.jwtObj.role.name;
+        if (role == "writer") {
             // @ts-ignore
-            res.render("writer_dashboard", {"fullname": req.jwtObj.full_name});
-            return; 
+            const articles = await writerController.getListArticleOfAuthor(req.jwtObj.id); 
+            const articlesStatus = (await writerController.getLatestStatusOfArticles(articles.map(item => item.id))).map(item => item.status); 
+
+            // @ts-ignore
+            res.render("writer_dashboard", { "fullname": req.jwtObj.full_name, "articles": articles, "articles_status": articlesStatus});
+            return;
         }
-    } 
-    
+    }
+
     // not writer
-    res.render("not_available",  {
+    res.render("not_available", {
         title: "Not Available | Lacainews",
         // @ts-ignore
         header: await headerGenerator(true, false, req.jwtObj.isPremium, -1, req.jwtObj.full_name),
         footer: await footerGenerator(),
-    }); 
+    });
 })
 
 router.get("/new-article", userMiddleware.authenticate, async (req: Request, res: Response) => {
     const categoryController = new CategoryController();
-    const tagController = new TagController(); 
+    const tagController = new TagController();
 
     // @ts-ignore
     console.log(req.jwtObj)
 
     // @ts-ignore
-    if (req.isAuth) { 
+    if (req.isAuth) {
         // @ts-ignore
-        const role = req.jwtObj.role.name; 
-        if (role == "writer") { 
+        const role = req.jwtObj.role.name;
+        if (role == "writer") {
             const categories = (await categoryController.getAllCategories()).map(item => item.name);
-            const tags = (await tagController.getAllTags()).map(item => item.name); 
+            const tags = (await tagController.getAllTags()).map(item => item.name);
 
             // @ts-ignore
-            res.render("writer_post", {"fullname": req.jwtObj.full_name, "categories": categories, "tags": tags});
-            return; 
+            res.render("writer_post", { "fullname": req.jwtObj.full_name, "categories": categories, "tags": tags });
+            return;
         }
-    } 
-    
+    }
+
     // not writer
-    res.render("not_available",  {
+    res.render("not_available", {
         title: "Not Available | Lacainews",
         // @ts-ignore
         header: await headerGenerator(true, false, req.jwtObj.isPremium, -1, req.jwtObj.full_name),
         footer: await footerGenerator(),
-    }); 
+    });
 })
 
 router.post("/upload", userMiddleware.authenticate, async (req: Request, res: Response) => {
+    // @ts-ignore
+    if (!req.isAuth) {
+        res.status(401); 
+        return; 
+    }
+    
+    // @ts-ignore
+    if (req.jwtObj.role.name != 'writer') { 
+        res.status(401); 
+        return; 
+    }
 
+    const { title, shortDescription, content, isPremium, category, tags } = req.body;
+    const writerController = new WriterController(); 
+    
+    // @ts-ignore
+    const result = await writerController.uploadArticle(req.jwtObj.id, title, 
+            shortDescription, content, category, tags, isPremium); 
+
+    console.log(result);
+
+    // @ts-ignore
+    res.status(result.status).json(result.message); 
 })
 
-export {router as writerRouter};
+export { router as writerRouter };
